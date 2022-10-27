8ca9a983923fe16c2d1171f38b7b5264b4c11da3
gastonfuentes <gastonnicolasfuentes@gmail.com>
2022-10-27 19:07:08 -0300

    funciones en ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
+import { Funciones } from "./typescript/Funciones";
 import { ObjetosLiterales } from "./typescript/ObjetosLiterales";
 import { TiposBásicos } from "./typescript/TiposBásicos";
-
 
 const App = () => {
   return (
     <div className="mt-2">
       <h1>Introducción a ts react</h1>
       <hr />
-      <TiposBásicos/>
+      <TiposBásicos />
       <hr />
       <ObjetosLiterales />
+      <hr />
+      <Funciones />
     </div>
-  )
-}
+  );
+};
 
 export default App;
-
